121458eed5770fad16bc5fff89299f4b526fac27
jjsemaan <jalal.semaan@parabim.com>
2024-03-03 20:59:40 +0000

    Add django project and about app

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# def index(request):
+    # return HttpResponse("Hello, World!")
+
 def index(request):
-    return HttpResponse("Hello, World!")
-    
+
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
